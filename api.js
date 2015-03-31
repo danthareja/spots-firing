2f7f3af8d475de6c9262d280ad464f2b868cd77f
Dan Thareja <danthareja@gmail.com>
2015-03-30 22:21:03 -0700

    ()

--- api.js
+++ api.js
@@ -1,33 +1,59 @@
 var MSW_API_KEY = "jx5HFZ5OyPH6zqKR9Pb8k230iSGymW2Q";
 var MSW_API_SECRET = "48Kx1256b8VpNBHLKh5pA3Q77BN2asqy";
-var mswSpot = '645';
-var json;
 
-$.ajax({
- 
-    url: 'http://magicseaweed.com/api/' + MSW_API_KEY + '/forecast/',
-    data: {
-        spot_id: mswSpot
-    },
-    type: "GET",
-    dataType : "jsonp",
-
-    success: function( response ) {
-        json = response;
-        console.log(response);
-    },
- 
-    error: function( xhr, status, errorThrown ) {
-        console.log( "Sorry, there was a problem!" );
-    },
- 
-    complete: function( xhr, status ) {
-        console.log("The request is complete");
-    }
-});
+var options = {
+  url: 'http://magicseaweed.com/api/' + MSW_API_KEY + '/forecast/',
+  data: {
+    spot_id: getSpot()
+  },
+  type: "GET",
+  dataType : "jsonp",
+};
 
 
+function callMSW() {
+  $.ajax(options)
+  .then(getCurrentForecast)
+  .then(checkFiveStars)
+  .done(function(rating) {
+    console.log(rating);
+  });
+}
+
+function getSpot() {
+  var spot;
+
+  do { spot = spotNumbers[Math.floor(Math.random() * spotNumbers.length)]; }
+  while (spotNumberHasBeenVisited[spot]);
+
+  spotNumberHasBeenVisited[spot] = true;
+  return spot;
+}
 
 
+function getCurrentForecast(weeklyForecast) {
+  var today = weeklyForecast.slice(0, 8);
+  var now = Math.round(Date.now() / 1000);
 
+  return today.reduce(function(current, forecast) {
+    return Math.abs(current.timestamp - now) < Math.abs(forecast.timestamp - now) ? current : forecast;
+  });
+}
 
+function checkSwell(forecast) {
+  if (forecast.solidRating === 5) {
+    displaySwell(forecast);
+  } else {
+    callMSW();
+  }
+}
+
+function handleError(err) {
+  console.log('ERROR GETTING FORECAST: ', err);
+}
+
+function resetSpots() {
+  spotNumbers.forEach(function(number) {
+    spotNumberHasBeenVisited[number] = false;
+  });
+}
